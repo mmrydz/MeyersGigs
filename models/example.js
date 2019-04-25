3be55fbf64046fc521aa7da7dbe2c2287c68d91a
William McFall <wmcfall2@gmail.com>
2019-04-25 13:41:36 -0400

    added sequelize model for user_info

--- models/example.js
+++ models/example.js
@@ -1,7 +1,47 @@
+// From the initial setup
+// module.exports = function(sequelize, DataTypes) {
+//   var Example = sequelize.define("Example", {
+//     text: DataTypes.STRING,
+//     description: DataTypes.TEXT
+//   });
+//   return Example;
+// };
+
+
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var users_info = sequelize.define("users_info", {
+    id:  {
+      type: DataTypes.INT,
+      primaryKey: true,
+      allowNull: false,
+    },
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: false,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      primaryKey: false,
+      allowNull: false,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      primaryKey: false,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      primaryKey: false,
+      allowNull: false,
+    },
+    personality: {
+      type: DataTypes.STRING,
+      primaryKey: false,
+      allowNull: true,
+    },
   });
-  return Example;
+  return users_info;
 };
+
