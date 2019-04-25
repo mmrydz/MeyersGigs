a0b1c36851adc97c3bd70262dc647ee9e0a82744
Meg Rydzewski <44847420+mmrydz@users.noreply.github.com>
2019-04-25 13:53:23 -0400

    Added schema for types_info table

--- models/schema.sql
+++ models/schema.sql
@@ -36,6 +36,12 @@ CREATE TABLE users_info(
    PRIMARY KEY(id)
 );
 
+CREATE TABLE types_info(
+    id INTEGER(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    personality VARCHAR(255),
+    description TEXT(500),
+    img_url TEXT(255)
+);
 
 DELIMITER ;;
 CREATE TRIGGER before_insert_users_info
