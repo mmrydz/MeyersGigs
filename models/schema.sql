365bbc984bf5f818ff97ddc1a2635e1c66b5af29
Meg Rydzewski <44847420+mmrydz@users.noreply.github.com>
2019-04-30 11:34:20 -0400

    Update schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -35,19 +35,16 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8;
 INSERT INTO `users` (`username`, `password`) VALUES ('John', 'password123');
 
 CREATE TABLE types_info(
-   id INTEGER(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   personality VARCHAR(255),
-   description TEXT(500),
-   img_url TEXT(255),
-   job_title_1 VARCHAR(255),
-   job_title_2 VARCHAR(255),
-   job_title_3 VARCHAR(255),
-   img_url_job_title_1 VARCHAR(255),
-   img_url_job_title_2 VARCHAR(255),
-   img_url_job_title_3 VARCHAR(255),
-   job_title_1_url VARCHAR(255),
-   job_title_2_url VARCHAR(255),
-   job_title_3_url VARCHAR(255)
+    id INTEGER(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    personality VARCHAR(255),
+    description TEXT(500),
+    img_url TEXT(255),
+    job_title_1 VARCHAR(255),
+    job_title_2 VARCHAR(255),
+    job_title_3 VARCHAR(255),
+    img_url_job_title_1 VARCHAR(255),
+    img_url_job_title_2 VARCHAR(255),
+    img_url_job_title_3 VARCHAR(255)
 );
 
 <<<<<<< HEAD
