569c0ccdc657f88ed19915801ec29ecfff6260c6
Meg Rydzewski <44847420+mmrydz@users.noreply.github.com>
2019-04-30 17:19:23 -0400

    Update schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -8,10 +8,12 @@ CREATE TABLE users(
    `username` VARCHAR(100),
    `password` VARCHAR(255),
    `personality` VARCHAR(255),
+	createdAt DATE DEFAULT false,
+	updatedAt DATE DEFAULT false,
    PRIMARY KEY(id)
 );
 
-CREATE TABLE types_info(
+CREATE TABLE types_infos(
     id INTEGER(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     personality VARCHAR(255),
     description TEXT(500),
@@ -24,6 +26,8 @@ CREATE TABLE types_info(
     job_title_3_img VARCHAR(255),
     job_title_1_url VARCHAR(255),
     job_title_2_url VARCHAR(255),
-    job_title_3_url VARCHAR(255)
+    job_title_3_url VARCHAR(255),
+	createdAt DATE DEFAULT false,
+	updatedAt DATE DEFAULT false
 );
 
